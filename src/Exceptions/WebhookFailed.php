cfde911f82ff97b033389f0aabec07d73f266520
Vladimir Shakurov <vladimir@shakurov.com>
2018-06-02 14:37:51 -0400

    Add missing function jobClassDoesNotExist

--- src/Exceptions/WebhookFailed.php
+++ src/Exceptions/WebhookFailed.php
@@ -21,6 +21,11 @@ class WebhookFailed extends Exception
     {
         return new static('The Coinbase Commerce webhook shared secret is not set. Make sure that the `coinbase.webhookSecfet` config key is set to the value you found on the Coinbase Commerce dashboard.');
     }
+    
+    public static function jobClassDoesNotExist(string $jobClass, CoinbaseWebhookCall $webhookCall)
+    {
+        return new static("Could not process webhook id `{$webhookCall->id}` of type `{$webhookCall->type} because the configured jobclass `$jobClass` does not exist.");
+    }
 
     public static function missingType()
     {
