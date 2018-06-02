acbce55f12e38804a36fffbe5149ee6e95d59e15
Vladimir Shakurov <shakurov@Vladimirs-MacBook-Air.local>
2018-06-02 13:32:51 -0400

    Change web route to api

--- src/Routes/api.php
+++ src/Routes/api.php
@@ -1,3 +1,5 @@
 <?php
 
-Route::post('coinbase/webhook', '\Shakurov\Coinbase\Http\Controllers\WebhookController')->name('coinbase-webhook');
+Route::group(['prefix' => 'api',  'middleware' => 'api'], function() {
+    Route::post('coinbase/webhook', '\Shakurov\Coinbase\Http\Controllers\WebhookController')->name('coinbase-webhook');
+});
